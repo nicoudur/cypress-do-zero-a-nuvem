fcc8b563ecd71defeaaf7d0e22526fe04c9872c2
Nicolas <nicolascoutoduarte@gmail.com>
2025-07-04 18:01:02 -0300

    seções 4 a 10 feitas

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -13,7 +13,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Silva')
     cy.get('#email').type('nicolas@exemplo.com')
     cy.get('#open-text-area').type(longText, { delay: 0 })
-    cy.contains('button','Enviar').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.success').should('be.visible')
   })
@@ -23,7 +23,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Silva')
     cy.get('#email').type('nicolas,exemplo.com')
     cy.get('#open-text-area').type('Excelente conteudo')
-    cy.contains('button','Enviar').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -32,47 +32,47 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#phone')
       .should('have.value', '')
       .type('teste teste')
-      
+
     cy.get('#phone').should('have.value', '')
   })
 
-  
+
   it('exibe mensagem de erro quando o telefone se torna obrigatório mas não e preenchido', () => {
     cy.get('#firstName').type('Nicolas')
     cy.get('#lastName').type('Silva')
     cy.get('#email').type('nicolas@exemplo.com')
     cy.get('#open-text-area').type('Excelente conteudo')
-    cy.get('#phone-checkbox').click()
-    cy.contains('button','Enviar').click()
+    cy.get('#phone-checkbox').check()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
 
-   it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
     cy.get('#firstName')
-    .type('Nicolas')
-    .should('have.value', 'Nicolas')
-    .clear()
-    .should('have.value', '')
+      .type('Nicolas')
+      .should('have.value', 'Nicolas')
+      .clear()
+      .should('have.value', '')
 
     cy.get('#lastName').type('Silva')
-    .should('have.value', 'Silva')
-    .clear()
-    .should('have.value', '')
+      .should('have.value', 'Silva')
+      .clear()
+      .should('have.value', '')
 
     cy.get('#email').type('nicolas@exemplo.com')
-    .should('have.value', 'nicolas@exemplo.com')
-    .clear()
-    .should('have.value', '')
+      .should('have.value', 'nicolas@exemplo.com')
+      .clear()
+      .should('have.value', '')
 
     cy.get('#phone').type('53999999999')
-    .should('have.value', '53999999999')
-    .clear()
-    .should('have.value', '')
+      .should('have.value', '53999999999')
+      .clear()
+      .should('have.value', '')
   })
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-    cy.contains('button','Enviar').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -89,5 +89,87 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success').should('be.visible')
   })
 
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product').select('YouTube')
+
+    cy.get('#product').should('have.value', 'youtube')
+
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product').select('mentoria')
+
+    cy.get('#product').should('have.value', 'mentoria')
+
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product').select(1)
+
+    cy.get('#product').should('have.value', 'blog')
+
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[value="feedback"]').check();
+
+    cy.get('input[value="feedback"]').should('be.checked')
+  })
+
+  it('marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]')
+      .each(typeOfService => {
+        cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+      })
+  })
+  it('marca ambos checkboxes, depois desmarca o último"', () => {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should('not.be.checked')
+  })
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/Teste.txt')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('Teste.txt')
+      })
+  })
+
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/Teste.txt', { action: 'drag-drop' })
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('Teste.txt')
+      })
+  })
+
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture("Teste.txt").as('sampleFile')
+    cy.get('#file-upload')
+      .selectFile('@sampleFile')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('Teste.txt')
+      })
+  })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr','target')
+      .click()
+
+     cy.contains('h1','CAC TAT - Política de Privacidade').should('be.visible')
+  })
 })
 
